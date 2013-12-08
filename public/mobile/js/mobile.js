8544a21c625a4e1af8f0f5a4782b9df1ac6992da
mockingbird87 <mockingbird87@gmail.com>
2013-12-08 16:47:50 +0100

    debugging mobile 2

--- public/mobile/js/mobile.js
+++ public/mobile/js/mobile.js
@@ -26,7 +26,7 @@ APP.main = function() {
 	// deleteCookie('test')
 
 
-	console.log( getCookie('test') )
+	console.log( getCookie('skipintro') )
 	// APP.introActions('init');
 
 	// return;
@@ -42,7 +42,7 @@ APP.main = function() {
 	// APP.introActions("init");
 	// return;
 	
-	APP.socket = io.connect('http://192.168.2.1:8888');
+	APP.socket = io.connect('http://54.194.105.54:8888');
 
 	APP.socket.on('paired', function (data) {
 		$$('a')[0].dispose();
